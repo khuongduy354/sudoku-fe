80a1caedb9ac7a01f92e1d76b2f0602f9d29c3b1
khuongduy354 <khuongduy354@gmail.com>
2022-01-26 20:46:44 +0700

    instruction in sudokusolver file

--- src/Sudoku/SudokuSolver/sudokuSolver.ts
+++ src/Sudoku/SudokuSolver/sudokuSolver.ts
@@ -1,3 +1,6 @@
 export function sudokuSolver(defaultSudoku: number[][]): [number[][]] {
   return [[]];
 }
+
+//tsc sudokuSolver.ts để dịch sang javascript
+//node sudokuSolver.js để chạy file javascript mới vừa dịch
