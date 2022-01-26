9661bbec2c0fc29fc90dbf5d2dd1160b576107c6
khuongduy354 <khuongduy354@gmail.com>
2022-01-26 21:12:04 +0700

    working formatter for sudoku
    format from string to array successfully

--- src/Sudoku/SudokuBoard/SudokuBoard.js
+++ src/Sudoku/SudokuBoard/SudokuBoard.js
@@ -1,43 +1,44 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.SudokuBoard = void 0;
 var SudokuBoard = /** @class */ (function () {
     //initiate empty board
     function SudokuBoard() {
         var _this = this;
-        this.gameState = ""; //game state in string form
-        this.parsedGameState = []; //game state in array form
+        this.stringGameState = ""; //game state in string form
+        this.arrayGameState = []; //game state in array form
         //getter for current game state in string
         this.getGameState = function () {
-            return _this.gameState;
+            return _this.stringGameState;
         };
         //getter for current game state in array
         this.getParsedGameState = function () {
-            return _this.parsedGameState;
+            return _this.arrayGameState;
         };
         //parse from string to 2D array
         this.parseGameState = function () {
-            var tempArr = [[]];
-            var currentState = _this.gameState;
-            var stringIndex = 0;
-            var row = 0;
-            var col = 0;
-            while (row < 9) {
-                //if hit \n, move to next row, reset to col 0
-                if (currentState[stringIndex] === "\n") {
-                    row++;
-                    col = 0;
+            //split into array of sudoku rows
+            var splittedRows = _this.stringGameState.split("\n");
+            splittedRows.pop(); //remove last "" in the array
+            //for each string, format it into a nested array of numbers
+            // result is a 2 dimensional array, with rows and columns
+            var arrayGameState = splittedRows.map(function (row) {
+                var tempArray = [];
+                for (var _i = 0, row_1 = row; _i < row_1.length; _i++) {
+                    var char = row_1[_i];
+                    tempArray.push(parseInt(char));
                 }
-                console.log(tempArr[row][col]);
-                //add string value to proper position in array
-                tempArr[row][col] = parseInt(currentState[stringIndex]);
-                col++;
-                stringIndex++;
-            }
-            _this.parsedGameState = tempArr;
+                return tempArray;
+            });
+            //update array game state
+            _this.arrayGameState = arrayGameState;
         };
+        //generate a random board game based on difficulty
+        this.generateBoard = function (difficulty) { };
+        //show full solution of current game
+        this.getSolution = function () { };
         for (var i = 0; i < 9; i++) {
-            this.gameState = this.gameState + "000000000\n";
+            this.stringGameState = this.stringGameState + "000000000\n";
         }
     }
     return SudokuBoard;
